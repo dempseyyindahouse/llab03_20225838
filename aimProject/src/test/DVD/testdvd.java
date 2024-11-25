667de6d43daa74be1bc447b5de3c743fbf6edbf7
Trung Hieu <167515573+dempseyyindahouse@users.noreply.github.com>
2024-11-25 23:50:07 +0700

    Update testdvd.java

--- aimProject/src/test/DVD/testdvd.java
+++ aimProject/src/test/DVD/testdvd.java
@@ -1,24 +1,40 @@
-package test.cart;
+package test.disc;
 
-import cart.Cart;
 import disc.DigitalVideoDisc;
 
-public class CartTest {
+public class TestPassingParameter {
+
 	public static void main(String[] args) {
-	    Cart myCart = new Cart();
-
-	    DigitalVideoDisc dvd1 = new DigitalVideoDisc("One Piece", "Anime", "Oda", 25, 1);
-	    DigitalVideoDisc dvd2 = new DigitalVideoDisc("Naruto", "Anime", "Kishimoto", 20, 2);
-	    DigitalVideoDisc dvd3 = new DigitalVideoDisc("Goku", "Action", "Toriyama", 22, 3);
-
-	    myCart.addDVD(dvd1, dvd2, dvd3);
-//	    myCart.addDVD(dvd2);
-//	    myCart.addDVD(dvd3);
-		myCart.removeDVD(dvd3);
-		myCart.print();
-	    
-	    myCart.searchById(2);
-	    myCart.searchByTittle("bo gia");
+		// TODO Auto-generated method stub
+		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+		
+		swap(jungleDVD, cinderellaDVD);
+//		swap(jungleDVD, cinderellaDVD, jungleDVD.getTitle(), cinderellaDVD.getTitle());
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+		System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+		
+		changeTitle(jungleDVD, cinderellaDVD.getTitle());
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
 		
 	}
+	
+	public static void swap(Object o1, Object o2) {
+		Object tmp = o1;
+		o1 = o2;
+		o2 = tmp;
+	}
+	
+	// rewrite swap
+//	public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2, String title1, String title2) {
+//	    dvd1.setTitle(title2);
+//	    dvd2.setTitle(title1);
+//	}
+	
+	public static void changeTitle(DigitalVideoDisc dvd, String title) {
+		String oldTitle = dvd.getTitle();
+		dvd.setTitle(title);
+		dvd = new DigitalVideoDisc(oldTitle);
+	}
+
 }
