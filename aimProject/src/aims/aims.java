e494735c81161ad61dcf232f8f728eb12cd58972
Trung Hieu <167515573+dempseyyindahouse@users.noreply.github.com>
2024-11-25 23:57:22 +0700

    aims.java

--- aimProject/src/aims/aims.java
+++ aimProject/src/aims/aims.java
@@ -4,24 +4,22 @@ import cart.Cart;
 import disc.DigitalVideoDisc;
 
 public class Aims {
-	  public static void main(String[] args) {
-	    Cart myCart = new Cart();
+    public static void main(String[] args) {
 
-	    DigitalVideoDisc dvd1 = new DigitalVideoDisc("One Piece", "Anime", "Oda", 25, 1);
-	    DigitalVideoDisc dvd2 = new DigitalVideoDisc("Naruto", "Anime", "Kishimoto", 20, 2);
-	    DigitalVideoDisc dvd3 = new DigitalVideoDisc("Goku", "Action", "Toriyama", 22, 3);
-	    DigitalVideoDisc dvd4 = new DigitalVideoDisc("Doraemon");
-	    DigitalVideoDisc dvd5 = new DigitalVideoDisc("Shin", "Cartoon", "xyz", 5);
+        Cart myCart = new Cart();
 
-	    myCart.addDVD(dvd1, dvd2, dvd3);
-//	    myCart.addDVD(dvd2);
-//	    myCart.addDVD(dvd3);
-		myCart.removeDVD(dvd3);
-		myCart.print();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("Avatar", "Science Fiction", "James Cameron", 162, 1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("The Godfather", "Crime", "Francis Ford Coppola", 175, 2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Inception", "Science Fiction", "Christopher Nolan", 148, 3);
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Toy Story");
+        DigitalVideoDisc dvd5 = new DigitalVideoDisc("Finding Nemo", "Animation", "Andrew Stanton", 100, 5);
 	    
-	    myCart.searchById(2);
-	    myCart.searchByTittle("bo gia");
-
-
-	  }
-	}
+        myCart.addDVD(dvd1);
+        myCart.addDVD(dvd2);
+        myCart.addDVD(dvd3);
+        myCart.removeDVD(dvd3);
+        myCart.print();
+        myCart.searchById(2);
+        myCart.searchByTitle("Finding Nemo");
+    }
+}
