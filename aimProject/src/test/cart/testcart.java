c9dd192b19c0b2b838b5537d96eea810dcd4c180
Trung Hieu <167515573+dempseyyindahouse@users.noreply.github.com>
2024-11-25 23:52:59 +0700

    testcart.java

--- aimProject/src/test/cart/testcart.java
+++ aimProject/src/test/cart/testcart.java
@@ -1,17 +1,22 @@
-public class Aims {
-    public static void main(String[] args) {
-      Cart myCart = new Cart();
-      DigitalVideoDisc dvd2 = new DigitalVideoDisc("Sasuke", "Anime", "Kishimoto", 20, 8.5f);
-      DigitalVideoDisc dvd3 = new DigitalVideoDisc("Vegeta", "Action", "Toriyama", 22, 9.0f);
-      DigitalVideoDisc dvd1 = new DigitalVideoDisc("One Piece", "Anime", "Oda", 20, 8.0f);
-  
-      myCart.addDVD(dvd1);
-      myCart.addDVD(dvd2);
-      myCart.addDVD(dvd3);
-  
-      myCart.print();
-  
-      myCart.removeDVD(dvd3);
-      myCart.print();
-    }
-  }
+package test.store;
+
+import store.Store;
+import cart.Cart;
+import disc.DigitalVideoDisc;
+
+public class StoreTest {
+	public static void main(String[] args) {
+	    Store store = new Store();
+
+      DigitalVideoDisc dvd1 = new DigitalVideoDisc("Sasuke", "Anime", "Kishimoto", 20, 8.5f);
+      DigitalVideoDisc dvd2 = new DigitalVideoDisc("Vegeta", "Action", "Toriyama", 22, 9.0f);
+      DigitalVideoDisc dvd3 = new DigitalVideoDisc("One Piece", "Anime", "Oda", 20, 8.0f);
+    
+        store.addDVD(dvd1);
+	    store.addDVD(dvd2);
+	    store.addDVD(dvd3);
+		store.removeDVD(dvd3);
+
+		
+	}
+}
