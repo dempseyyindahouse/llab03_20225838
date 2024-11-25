02780535b04e53724e5952427f5343889cd29835
Trung Hieu <167515573+dempseyyindahouse@users.noreply.github.com>
2024-11-25 23:48:02 +0700

    dvd.java

--- aimProject/src/DVD/dvd.java
+++ aimProject/src/DVD/dvd.java
@@ -1,83 +1,40 @@
-package disc;
+package test.disc;
 
-public class DigitalVideoDisc {
-	  private static int nbDigitalVideoDiscs = 0;
-	  private int id;
-	  private String title;
-	  private String category;
-	  private String director;
-	  private int length;
-	  private float cost;
-	  
-	  public String getTitle() {
-	    return title;
-	  }
-	  public String getCategory() {
-	    return category;
-	  }
-	  public String getDirector() {
-	    return director;
-	  }
-	  public int getLength() {
-	    return length;
-	  }
-	  public float getCost() {
-	    return cost;
-	  }
-	  public int getId() {
-		return id;
-	  }
-	  
-	  
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public void setDirector(String director) {
-		this.director = director;
-	}
-	public void setLength(int length) {
-		this.length = length;
-	}
-	public void setCost(float cost) {
-		this.cost = cost;
-	}
-	
-    // Constructor full
-    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.length = length;
-        this.cost = cost;
-    }
+import disc.DigitalVideoDisc;
 
-    public DigitalVideoDisc(String title) {
-        this(title, null, null, 0, 0.0f); 
-    }
-	public DigitalVideoDisc(String title, String category, float cost) {
-		this(title, category, null, 0, cost); 
-	}
-	public DigitalVideoDisc(String title, String category, String director, float cost) {
-		this(title, category, category, 0, cost); 
+public class TestPassingParameter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+		
+		swap(jungleDVD, cinderellaDVD);
+//		swap(jungleDVD, cinderellaDVD, jungleDVD.getTitle(), cinderellaDVD.getTitle());
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+		System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+		
+		changeTitle(jungleDVD, cinderellaDVD.getTitle());
+		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+		
 	}
 	
-	
-	// in thong tin DVD
-	public String toString() {
-		return "DVD - " + this.title + " - " + this.category + " - " 
-				+ this.category + " - "+ this.length + " minutes: " 
-				+ this.cost + "$";
+	public static void swap(Object o1, Object o2) {
+		Object tmp = o1;
+		o1 = o2;
+		o2 = tmp;
 	}
 	
-	// check title
-	public boolean isMatch(String title) {
-		return this.title.equals(title);
+	// rewrite swap
+//	public static void swap(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2, String title1, String title2) {
+//	    dvd1.setTitle(title2);
+//	    dvd2.setTitle(title1);
+//	}
+	
+	public static void changeTitle(DigitalVideoDisc dvd, String title) {
+		String oldTitle = dvd.getTitle();
+		dvd.setTitle(title);
+		dvd = new DigitalVideoDisc(oldTitle);
 	}
-   
-	  
+
 }
