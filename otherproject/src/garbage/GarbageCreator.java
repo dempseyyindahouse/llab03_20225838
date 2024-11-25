ac05dd07428a5e96375a4680aeff240ee026b5d7
Trung Hieu <167515573+dempseyyindahouse@users.noreply.github.com>
2024-11-25 23:04:16 +0700

    GarbageCreator.java

--- otherproject/src/garbage/GarbageCreator.java
+++ otherproject/src/garbage/GarbageCreator.java
@@ -1,31 +1,29 @@
 package garbage;
-
-import java.io.IOException; 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 public class GarbageCreator {
-	public static void main(String[] args) {
-		String filename = "C:\\Windows\\System32\\notepad.exe";;
-		byte[] inputBytes = { 0 };
-		long startTime, endTime;
-		
-		try {
-			inputBytes = Files.readAllBytes(Paths.get(filename));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace(); // In ra thông báo lỗi nếu có ngoại lệ
-		}
-		startTime = System.currentTimeMillis();
-//		String outputString = "";
-//		for(byte b : inputBytes) {
-//			outputString += (char)b;
-//		}
-		StringBuilder outputStringBuilder = new StringBuilder();
-		for(byte b : inputBytes) {
-			outputStringBuilder.append((char)b);
-		}
-		endTime = System.currentTimeMillis();
-		System.out.println(endTime - startTime);
-	}
+    public static void main(String[] args) {
+        String filename = "C:\\Windows\\System32\\notepad.exe";
+        List<Byte> byteList = new ArrayList<>();
+        long startTime, endTime;
+        try (FileInputStream fis = new FileInputStream(filename)) {
+            int byteRead;
+            while ((byteRead = fis.read()) != -1) {
+                byteList.add((byte) byteRead);
+            }
+        } catch (IOException e) {
+            e.printStackTrace(); 
+        }
+        startTime = System.currentTimeMillis();
+        byte[] inputBytes = new byte[byteList.size()];
+        for (int i = 0; i < byteList.size(); i++) {
+            inputBytes[i] = byteList.get(i);
+        }
+        String outputString = new String(inputBytes);
+        endTime = System.currentTimeMillis();
+        System.out.println((endTime - startTime) );
+        System.out.println(outputString.length());
+    }
 }
